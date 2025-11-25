794ef0fe700956a1fd130504ccd81a3d4f29bc4e
RahulShirke28804 <rahulshirke28804@gmail.com>
2025-11-25 12:38:07 +0530

    Add files via upload

---
+++ ReverseDigits1.java
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     RerverseDigits1.java
+//  Description :   Display the digits in reverse order
+//  Author :        Rahul Ajay Shirke
+//  Date :          25-10-2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Digit
+{
+    public void DisplayDigits(int iNo)
+    {
+        int iDigit = 0;
+        
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            System.out.println(iDigit);
+            iNo = iNo / 10;
+        }
+    }
+}
+
+class ReverseDigits1
+{
+    public static void main(String A[])
+    {
+        int iValue = 0;
+
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();        
+        
+
+        Digit dobj = new Digit();
+
+        dobj.DisplayDigits(iValue);
+
+        // Important
+        sobj = null;
+        dobj = null;
+
+        System.gc();
+    }
+}
