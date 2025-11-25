0b8148b1e74c5d4d44d2a3d773b6e108ee2bed98
RahulShirke28804 <rahulshirke28804@gmail.com>
2025-11-25 12:37:47 +0530

    Add files via upload

---
+++ Factorial3.java
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Factorial3.java
+//  Description :   Calculate the factorial of a given number
+//  Author :        Rahul Ajay Shirke
+//  Date :          25-10-2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Number
+{
+    public long CalculateFactorial(int iNo)
+    {
+        int i = 1;
+        long iFact = 1;
+
+        if(iNo < 0)
+        {
+            return 1;
+        }
+
+        while (i <= iNo) 
+        {
+            iFact = iFact * i;
+            i++;
+        }
+        
+        return iFact;
+    }
+} // End of Number class
+
+class Factorial3
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+        long iRet = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();        
+        
+
+        Number nobj = new Number();
+
+        iRet = nobj.CalculateFactorial(iValue);
+
+        System.out.println("Factorial is : "+iRet);
+
+        // Important
+        sobj = null;
+        nobj = null;
+
+        System.gc();
+    }
+}
